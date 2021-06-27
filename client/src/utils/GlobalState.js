e5b0c1db54ee1cbdcfb8a88d654183fdb95b81d4
sg0703 <samgates@Sams-MBP.attlocal.net>
2021-06-27 10:49:08 -0400

    Created new redux store in GlobalState, default state in reducers

--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -1,3 +1,15 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+
+// Import Redux store from the file we created in /utils
+import store from './store';
+
+export default function StoreProvider(props) {
+  // Return the provider component provided by react-redux
+  return <Provider store={store} {...props} />;
+}
+
+/** 
 import React, { createContext, useContext } from "react";
 import { useProductReducer } from './reducers'
 
@@ -21,3 +33,4 @@ const useStoreContext = () => {
 };
 
 export { StoreProvider, useStoreContext };
+*/
--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -1,4 +1,3 @@
-import { useReducer } from 'react';
 import {
   UPDATE_PRODUCTS,
   ADD_TO_CART,
@@ -11,11 +10,16 @@ import {
   TOGGLE_CART,
 } from './actions';
 
-// TODO: To get a better understand of how a reducer works - add comments to the various actions in the reducer
-export const reducer = (state, action) => {
+const initialState = {
+  products: [],
+  cart: [],
+  cartOpen: false,
+  categories: [],
+  currentCategory: '',
+};
+
+export const reducer = (state = initialState, action) => {
   switch (action.type) {
-    // TODO: Add a comment describing the functionality of the UPDATE_PRODUCTS case
-    // Your comment here
     case UPDATE_PRODUCTS:
       return {
         ...state,
@@ -34,8 +38,6 @@ export const reducer = (state, action) => {
         ...state,
         cart: [...state.cart, ...action.products],
       };
-    // TODO: Add a comment describing the functionality of the UPDATE_CART_QUANTITY case
-    // Your comment here
     case UPDATE_CART_QUANTITY:
       return {
         ...state,
@@ -47,9 +49,6 @@ export const reducer = (state, action) => {
           return product;
         }),
       };
-
-    // TODO: Add a comment describing the functionality of the REMOVE_FROM_CART case
-    // Your comment here
     case REMOVE_FROM_CART:
       let newState = state.cart.filter((product) => {
         return product._id !== action._id;
@@ -86,13 +85,12 @@ export const reducer = (state, action) => {
         currentCategory: action.currentCategory,
       };
 
-    // TODO: Add a comment describing what the default case is for
-    // Your comment here
     default:
       return state;
   }
 };
 
-export function useProductReducer(initialState) {
-  return useReducer(reducer, initialState);
-}
+// no longer needed
+//export function useProductReducer(initialState) {
+//  return useReducer(reducer, initialState);
+//}
