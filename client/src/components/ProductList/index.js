d9e53cbc3426efc49381cf94b092b7b0ce8f91fd
sg0703 <samgates@Sams-MBP.attlocal.net>
2021-06-27 11:08:48 -0400

    Finished with component refactor

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -1,14 +1,21 @@
 import React, { useEffect } from 'react';
 import ProductItem from '../ProductItem';
-import { useStoreContext } from '../../utils/GlobalState';
+//import { useStoreContext } from '../../utils/GlobalState';
 import { UPDATE_PRODUCTS } from '../../utils/actions';
 import { useQuery } from '@apollo/client';
 import { QUERY_PRODUCTS } from '../../utils/queries';
 import { idbPromise } from '../../utils/helpers';
 import spinner from '../../assets/spinner.gif';
 
+// new stuff for redux 
+import { useDispatch, useSelector } from 'react-redux';
+
 function ProductList() {
-  const [state, dispatch] = useStoreContext();
+ // const [state, dispatch] = useStoreContext();
+
+  // redux refactor 
+  const dispatch = useDispatch();
+  const state = useSelector((state) => state);
 
   const { currentCategory } = state;
 
